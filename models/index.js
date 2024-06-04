300a08c99f92d100bcbc6f4c9ff4c1340289b85d
Giridharan <giridharan.ranganathan.99@gmail.com>
2024-06-04 14:21:24 +0530

    connection check

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://giridharanranganathan99:tuNCoCc2FBZj8idf@cluster0.5yeep8u.mongodb.net/";
+const uri = "mongodb+srv://giridharanranganathan99:tuNCoCc2FBZj8idf@cluster0.5yeep8u.mongodb.net/test";
 
 
 function main() {
